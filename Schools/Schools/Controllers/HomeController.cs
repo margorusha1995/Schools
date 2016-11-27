fb2a1f1ebc9e4b98de669c2fc66355e27f1857a9
xorost54 <xorost54@gmail.com>
2016-11-27 16:51:37 +0300

    Google распознавание, и установка меток

--- Schools/Schools/Controllers/HomeController.cs
+++ Schools/Schools/Controllers/HomeController.cs
@@ -27,15 +27,53 @@ namespace Schools.Controllers
 
         public IActionResult GoogleMaps()
         {
-            return View();
+            List<Structure> data = new List<Structure>();
+            var result = from schoolAddress in context.rbd_SchoolAddress
+                         join school in context.rbd_Schools on schoolAddress.SchoolID equals school.SchoolID
+                         join address in context.rbd_Address on schoolAddress.AddressID equals address.AddressID
+                         join locality in context.rbdc_LocalityTypes on address.LocalityTypeID equals locality.LocalityTypeID
+                         join street in context.rbdc_StreetTypes on address.StreetTypeID equals street.StreetTypeID
+                         join township in context.rbd_Townships on address.TownshipID equals township.TownshipID
+                         join building in context.rbdc_BuildingTypes on address.BuildingTypeID equals building.BuildingTypeID
+                         where schoolAddress.AddressTypeID == 1
+                         select new
+                         {
+                             ID = address.AddressID,
+                             Name = school.SchoolName,
+                             ZipCode = address.ZipCode,
+                             LocalityName = address.LocalityName,
+                             StreetName = address.StreetName,
+                             BuildingNumber = address.BuildingNumber,
+                             LocalityTypeName = locality.LocalityTypeName,
+                             StreetTypeName = street.StreetTypeName,
+                             TownshipName = township.TownshipName,
+                             BuildingTypeName = building.BuildingTypeName
+                         };
+            int i = 0;
+            foreach (var item in result)
+            {
+                i++;
+                data.Add(new Structure()
+                {
+                    address = item.TownshipName + ", " + item.LocalityTypeName + " " + item.LocalityName + ", " + item.StreetTypeName + " " + item.StreetName + ", " + item.BuildingNumber,
+                    id = item.ID.ToString(),
+                    name = item.Name
+                });
+
+                if (i > 10)
+                {
+                    break;
+                }
+            }
+
+            ViewBag.Addresses = data;
+
+            return View(); 
         }
 
         public IActionResult Index()
         {
             List<Structure> data = new List<Structure>();
-
-
-
             var result = from schoolAddress in context.rbd_SchoolAddress
                          join school in context.rbd_Schools on schoolAddress.SchoolID equals school.SchoolID
                          join address in context.rbd_Address on schoolAddress.AddressID equals address.AddressID
